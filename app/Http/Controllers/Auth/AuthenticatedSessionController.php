d11e0aef7fa6b8deb3c33e45dacdfc38481dcf5d
xrdn00 <reydan39@gmail.com>
2025-08-11 02:50:06 +0800

    include rls updates within request

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -30,6 +31,9 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
+
+        // Ensure RLS updates within this request succeed by setting the user id GUC
+        DB::statement("select set_config('app.user_id', ?, false)", [strval(Auth::id())]);
 
         $request->session()->regenerate();
 
